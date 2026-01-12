44f7758b180f51ec12b003bc0ccb25ad9afebc9d
MilanKuja <ilmilan95@gmail.com>
2026-01-12 07:12:32 -0600

    pocetak projekta

---
+++ src/main/java/DriverSetup/Driver.java
@@ -0,0 +1,11 @@
+package DriverSetup;
+
+public class Driver {
+    private static final String CHROME_DRIVER = "webdriver.chrome.driver";
+
+    private static WebDriver driver;
+
+    protected static void setDriver(WebDriver driver) {
+
+    }
+}
